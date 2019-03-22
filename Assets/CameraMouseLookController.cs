3381d4922447cbaa7544447b565f4099da30e556
Macarena Haspert <hmacarena@gmail.com>
2019-03-22 10:20:36 -0300

    lots of changes

---
+++ Assets/CameraMouseLookController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CameraMouseLookController : MonoBehaviour
+{
+    private Vector2 mouseLook;
+    private Vector2 smooth;
+
+    [SerializeField]
+    private float sensitivity = 5.0f;
+
+    [SerializeField]
+    private float smoothing = 2.0f;
+
+    [SerializeField]
+    private GameObject character;
+
+    void Update()
+    {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+        smooth.x = Mathf.Lerp(smooth.x, md.x, 1f / smoothing);
+        smooth.y = Mathf.Lerp(smooth.y, md.y, 1f / smoothing);
+        mouseLook += smooth;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
---
+++ Assets/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
